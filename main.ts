3bb8e3ecdf716128a2944f17235ffdb948fb5f73
ardofulian <careday17@gmail.com>
2025-09-04 07:49:45 +0700

    update version

--- main.ts
+++ main.ts
@@ -1,7 +1,6 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import { oakCors } from "https://deno.land/x/cors/mod.ts";
 
-// Baca data dari file JSON
 const schedulesData = JSON.parse(
   await Deno.readTextFile("./data/schedules.json"),
 );
@@ -13,7 +12,6 @@ router
   })
   .get("/download/:version", async (context) => {
     const version = context.params.version.replace(".apk", "");
-    // Sanitize version parameter to ensure it's a float only
     if (!/^\d+(\.\d+)?$/.test(version)) {
       context.response.status = 400;
       context.response.body = "Invalid version format. Must be a float value.";
@@ -29,7 +27,7 @@ router
   });
 
 const app = new Application();
-app.use(oakCors()); // Enable CORS for all routes
+app.use(oakCors());
 app.use(router.routes());
 app.use(router.allowedMethods());
 
